a2c268e5a2af866ae58c1e47df35dd6dd3b758a0
PoojaMurarka <pooja.murarka@kony.com>
2015-06-04 22:38:04 +0530

    commiting student result js,form

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -14,6 +14,24 @@
          activate_page("#uib_page_1"); 
     });
     
+        /* button  #btnLogin_Login */
+    $(document).on("click", "#btnLogin_Login", function(evt)
+    {
+         activate_page("#ScreenDashboard"); 
+    });
+    
+        /* button  #btn11 */
+    $(document).on("click", "#btn11", function(evt)
+    {
+         activate_page("#ScreenLogin"); 
+    });
+    
+        /* button  #btn22 */
+    $(document).on("click", "#btn22", function(evt)
+    {
+         activate_page("#StudentResult"); 
+    });
+    
     }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
---
+++ www/js/studentResult.js
@@ -0,0 +1,7 @@
+function StudentResultCnt($scope)
+            {
+                //An JSON array to hold the student list.
+                $scope.students = [{subjectCode: 'sub0',suject : 'Science' ,grade : 'A+' },
+                                  {subjectCode: 'sub01',suject : 'Math' ,grade : 'B'},{subjectCode: 'sub02',suject : 'English' ,grade : 'B+'}];
+                 
+            } 
